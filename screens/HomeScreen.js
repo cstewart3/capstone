8ddfc427aa8132d78aaec8df2da7b1cd5f11762d
Alexi Doak <alexidoak@gmail.com>
2018-10-02 20:32:23 -0400

    This is the first commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -46,7 +46,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload!
+              OK!
             </Text>
           </View>
 
