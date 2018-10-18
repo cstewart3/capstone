ba6a36fc837ea00f3f8c74e02bf60c64dd0e58fd
Charlie Stewart <cstewart3>
2018-10-18 18:00:02 -0400

    map filter

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,10 +1,36 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Picker, Button, Modal, TouchableHighlight } from 'react-native';
 import Expo from 'expo';
 
 
 
 export default class App extends React.Component {
+  static navigationOptions = {
+    title: 'Map View',
+  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pickerSelection: 'All Stops',
+      pickerDisplayed: false
+    }
+  }
+
+  setPickerValue(newValue) {
+    this.setState({
+      pickerSelection: newValue
+    })
+
+    this.togglePicker();
+  }
+
+  togglePicker() {
+    this.setState({
+      pickerDisplayed: !this.state.pickerDisplayed
+    })
+  }
+
 
   state = {
     location: null,
@@ -28,10 +54,61 @@ componentDidMount() {
 
   render() {
 
+    const pickerValues = [
+      {
+        title: 'All Stops',
+        value: 'All Stops'
+      },
+      {
+        title: "Lincoln's Visit",
+        value: "Lincoln's Visit"
+      },
+      {
+        title: 'Citizens and the Battle',
+        value: 'Citizens and the Battle'
+      },
+      {
+        title: "Gettysburg's Black History",
+        value: "Gettysburg's Black History"
+      },
+      {
+        title: "Early Gettysburg",
+        value: "Early Gettysburg"
+      }
+    ]
+
+
     if (!this.state.location) {
       return (<View />)
     }
     return (
+      <React.Fragment>
+
+        <View style={{height: 0}} />
+          <Button onPress={() => this.togglePicker()} title={ "Map Filters" }/>
+           <Text style={{textAlignVertical: "center",textAlign: "center"}}>Currently Showing { this.state.pickerSelection }</Text>
+          <Modal visible={this.state.pickerDisplayed} animationType={"slide"} transparent={true}>
+            <View style={{ margin: 20, padding: 20,
+              backgroundColor: '#efefef',
+              bottom: 20,
+              left: 20,
+              right: 20,
+              alignItems: 'center',
+              position: 'absolute' }}>
+              <Text style={{fontWeight: "bold"}}>Filters</Text>
+              { pickerValues.map((value, index) => {
+                return <TouchableHighlight key={index} onPress={() => this.setPickerValue(value.value)} style={{ paddingTop: 4, paddingBottom: 4 }}>
+                    <Text style={{color: '#5a5f66'}}>{ value.title }</Text>
+                  </TouchableHighlight>
+              })}
+
+
+              <TouchableHighlight onPress={() => this.togglePicker()} style={{ paddingTop: 4, paddingBottom: 4 }}>
+                <Text style={{ color: '#999' }}>Cancel</Text>
+              </TouchableHighlight>
+            </View>
+          </Modal>
+
       <Expo.MapView style = {{flex: 1}} provider = {Expo.MapView.PROVIDER_GOOGLE}
       //<Expo.MapView style = {{flex: 1}}
       initialRegion = {{
@@ -44,14 +121,14 @@ componentDidMount() {
 
       }}>
 
-      // Mateus and Charlie created these markers!! Yay Alexi and Nick are happy!!
       <Expo.MapView.Marker coordinate = {this.state.location.coords} title = {"You are here cool person"} pinColor = {"#f442f1"} />
       <Expo.MapView.Marker coordinate = {{latitude: 39.821205, longitude: -77.232254}} title = {"Scary Place"} />
       <Expo.MapView.Marker coordinate = {{latitude: 39.830947, longitude: -77.231133}} title = {"Lincoln Square"} description = {"The Circle of Gettysburg"} pinColor = {"#ede02d"}/>
       <Expo.MapView.Marker coordinate = {{latitude: 39.832010, longitude: -77.231280}} title = {"Lincoln Diner"} description = {"Questionable food"} pinColor = {"#1fddf2"}/>
-
+      <Expo.MapView.Marker coordinate = {{latitude: 39.831058, longitude: -77.230580}} title = {"David Wills House"} description = {"Lincoln made final adjustments to the Gettysburg Address here"} pinColor = {"#1fddf2"}/>
 
       </Expo.MapView>
+    </React.Fragment>
     );
   }
 }
